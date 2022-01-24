5a973e668cc5338501a9f3e6b73cfa8cb03c373f
Avi <you@example.com>
2022-01-24 11:27:56 +0200

    first step solved

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-// rawInput = `target area: x=20..30, y=-10..-5`;
+rawInput = `target area: x=20..30, y=-10..-5`;
 
 const input = (() => {
     const values = rawInput
@@ -7,35 +7,70 @@ const input = (() => {
         .map(coord => coord.substring(2))
         .map(coord => coord.split('..'))
         .map(([min, max]) => [+min, +max]);
-    
+
     const [[minX, maxX], [minY, maxY]] = values;
-    const yCorrection = minY + 1;
 
     return {
         minX,
         maxX,
-        minY: minY + yCorrection,
-        maxY: maxY + yCorrection,
-        yCorrection
+        minY: minY,
+        maxY: maxY
     };
 })();
 
-function getMinThrowX(targetX) {
-    return Math.ceil(Math.abs(
-        (1 - Math.sqrt(1 + 8 * targetX)) / 2
-    ));
+function canReachY(velocity, steps) {
+    function canDescendToPoint(startY, targetY) {
+        const result = Math.sqrt(1 + (8 * (startY - targetY)));
+        return result === Math.round(result) && result % 2 === 1 && result;
+    }
+
+    const peakY = ((velocity * velocity) + velocity) / 2;
+    const remainingSteps = steps - velocity;
+
+    for (let y = input.minY; y <= input.maxY; y++) {
+        const result = canDescendToPoint(peakY, y);
+        if (result !== false && result >= remainingSteps) {
+            return peakY;
+        }
+    }
+
+    return false;
 }
 
 const possibleNumOfSteps = (() => {
+    function getMinThrowX(targetX) {
+        return Math.ceil(Math.abs(
+            (1 - Math.sqrt(1 + 8 * targetX)) / 2
+        ));
+    }
+
     const minSteps = getMinThrowX(input.minX);
     const maxSteps = getMinThrowX(input.maxX);
     const arr = [];
 
-    for (let i = maxSteps; i >= minSteps ; i--) {
+    for (let i = maxSteps; i >= minSteps; i--) {
         arr.push(i);
     }
 
     return arr;
 })();
 
-console.log(possibleNumOfSteps);
+const highestThrow = (() => {
+    let count = 0;
+    let highest = Number.MIN_VALUE;
+    for (const numOfSteps of possibleNumOfSteps) {
+        for (let i = 1; i < 1000; i++) {
+            let currHighest = canReachY(i, numOfSteps);
+
+            if (currHighest !== false) {
+                // console.log('got', currHighest, 'for', { currVelocity:i, numOfSteps });
+                highest = Math.max(highest, currHighest);
+                count++;
+            }
+        }
+    }
+
+    return { highest, count };
+})();
+
+console.log(highestThrow);
